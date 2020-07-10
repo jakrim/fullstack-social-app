328cd953220bd2a083ecd55222601cb70ca664dd
Jesse Krim <jesse.krim@gmail.com>
2020-07-09 23:51:28 -0400

    first commit

---
+++ functions/index.js
@@ -0,0 +1,54 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+var serviceAccount = require('../serviceAccountKey.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: 'https://social-app-e8446.firebaseio.com',
+});
+// admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then((data) => {
+      let screams = [];
+      data.forEach((doc) => {
+        screams.push({
+          screamId: doc.id,
+          ...doc.data(),
+        });
+      });
+
+      return res.json(screams);
+    })
+    .catch((err) => console.error(err));
+});
+
+app.post('/scream', (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  };
+
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then((doc) => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch((err) => {
+      res.status(500).json({ error: 'something went wrong' });
+      console.error(err);
+    });
+});
+
+exports.api = functions.https.onRequest(app);
